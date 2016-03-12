ecffbe55c5c3ea867f7c80bf02224f47a62b6280
portosam <samu54h@yahoo.it>
2016-03-12 20:45:41 +0100

    Update questions.js

--- js/questions.js
+++ js/questions.js
@@ -1,27 +1,51 @@
 var domande = [
 	/*0*/	"0",
-	/*1*/	"1",
-    /*2*/	"Quale lapide ha qualcosa in comune con il film ”Caccia ad ottobre rosso” e parla di un nuovo mezzo marittimo?",
-    /*3*/	"Quante e quali  sono le lapidi che sono “decorate” con l'immagine di un fucile?",
-    /*4*/	"In quante lapidi al posto del fucile hanno messo la spada?",
-    /*5*/	"Trova il caporale del 35° regg. Fanteria disperso durante il fatto d'armi",
-    /*6*/	"Quali sono i simboli della guerra più ricorrenti nelle decorazioni delle lapidi?",
-    /*7*/	"Nel nostro cimitero sono sepolti solo i soldati italiani?",
-    /*8*/	"Quale particolare arma chimica è stata utilizzata nella prima guerra mondiale? Di quale patriota ha causato la morte?",
-    /*9*/	"Chi insegnò ai suoi compagni la lingua tedesca e fu tra i primi a prestare giuramento nella Guardia Nazionale di Cremona?",
-    /*10*/	"Rinunciò alla sua giovinezza per un'Italia più grande e morì lontano dai genitori. Chi è ?",
-    /*11*/	"Chi fu il sottotenente dei bersaglieri che cadde combattendo sul  Carnizza?",
-    /*12*/	"Chi è il più giovane tra i caduti della prima guerra mondiale nelle fotografie della mostra?",
-    /*13*/	"Chi è rappresentato sulla sua lapide portando la bandiera italiana nonostante sia nato all'estero?",
-    /*14*/	"Durante la prima guerra mondiale scomparve nel mare insieme al sommergibile nel quale svolgeva il compito di secondo capo meccanico. Di chi si tratta?",
-    /*15*/	"Qual è il personaggio assistito da “Miss uragano” durante la battaglia del Volturno?",
-    /*16*/	"Tra le foto delle lapidi, ce n'è una in memoria di due fratelli, entrambi nati a Cremona verso la fine del XIX secolo: uno è stato ucciso da una granata nemica, l'altro è deceduto a causa di una malattia. Sapresti trovarla?",
-    /*17*/	"Chi dopo 30 mesi di guerra morì dopo tanti sacrifici il 30 maggio del 1919 all'ospedale Manini?",
-    /*18*/	"Schierato per un esercito rivale, disertò per combattere con l'Italia. Non conosciamo il suo volto. Chi è ?",
-    /*19*/	"Trova quante volte, con i rispettivi nomi dei caduti, è presente sulle lapidi il simbolo dei Savoia rappresentato da una croce e una corona",
-    /*20*/	"Lavorò come volontario insieme alle crocerossine cremonesi nel 1916, chi fu?",
-    /*21*/	"Chi, molto caro alle sue sorelle, è raffigurato con l'elmo di Scipio?",
-    /*22*/	"Il giusto dell'anima è colui che si dona al prossimo, chi è degno di questo premio?",
-    /*23*/	"Chi morì con il grado maggiore di cavalleria dopo aver fatto il soldato per 15 anni?",
-    /*24*/	"Quale lapide raffigura due giberne appese allo scudo dei Savoia?",
+	/*1*/	["1",
+			"risposta giusta"], //risposta giusta
+    /*2*/	["Quale lapide ha qualcosa in comune con il film ”Caccia ad ottobre rosso” e parla di un nuovo mezzo marittimo?",
+    			"risposta giusta"], //risposta giusta
+    /*3*/	["Quante e quali  sono le lapidi che sono “decorate” con l'immagine di un fucile?",
+    			"risposta giusta"], //risposta giusta
+    /*4*/	["In quante lapidi al posto del fucile hanno messo la spada?",
+    	 		"risposta giusta"], //risposta giusta
+    /*5*/	["Trova il caporale del 35° regg. Fanteria disperso durante il fatto d'armi",
+    			"risposta giusta"], //risposta giusta
+    /*6*/	["Quali sono i simboli della guerra più ricorrenti nelle decorazioni delle lapidi?",
+    			"risposta giusta"], //risposta giusta
+    /*7*/	["Nel nostro cimitero sono sepolti solo i soldati italiani?",
+    		 	"risposta giusta"], //risposta giusta
+    /*8*/	["Quale particolare arma chimica è stata utilizzata nella prima guerra mondiale? Di quale patriota ha causato la morte?",
+    			"risposta giusta"], //risposta giusta
+    /*9*/	["Chi insegnò ai suoi compagni la lingua tedesca e fu tra i primi a prestare giuramento nella Guardia Nazionale di Cremona?",
+    			"risposta giusta"], //risposta giusta
+    /*10*/	["Rinunciò alla sua giovinezza per un'Italia più grande e morì lontano dai genitori. Chi è ?",
+    			"risposta giusta"], //risposta giusta
+    /*11*/	["Chi fu il sottotenente dei bersaglieri che cadde combattendo sul Carnizza?",
+    			"risposta giusta"], //risposta giusta
+    /*12*/	["Chi è il più giovane tra i caduti della prima guerra mondiale nelle fotografie della mostra?",
+    			"risposta giusta"], //risposta giusta
+    /*13*/	["Chi è rappresentato sulla sua lapide portando la bandiera italiana nonostante sia nato all'estero?",
+    			"risposta giusta"], //risposta giusta
+    /*14*/	["Durante la prima guerra mondiale scomparve nel mare insieme al sommergibile nel quale svolgeva il compito di secondo capo meccanico. Di chi si tratta?",
+    			"risposta giusta"], //risposta giusta
+    /*15*/	["Qual è il personaggio assistito da “Miss uragano” durante la battaglia del Volturno?",
+    			"risposta giusta"], //risposta giusta
+    /*16*/	["Tra le foto delle lapidi, ce n'è una in memoria di due fratelli, entrambi nati a Cremona verso la fine del XIX secolo: uno è stato ucciso da una granata nemica, l'altro è deceduto a causa di una malattia. Sapresti trovarla?",
+    			"risposta giusta"], //risposta giusta
+    /*17*/	["Chi dopo 30 mesi di guerra morì dopo tanti sacrifici il 30 maggio del 1919 all'ospedale Manini?",
+    			"risposta giusta"], //risposta giusta
+    /*18*/	["Schierato per un esercito rivale, disertò per combattere con l'Italia. Non conosciamo il suo volto. Chi è ?",
+    			"risposta giusta"], //risposta giusta
+    /*19*/	["Trova quante volte, con i rispettivi nomi dei caduti, è presente sulle lapidi il simbolo dei Savoia rappresentato da una croce e una corona",
+    			"risposta giusta"], //risposta giusta
+    /*20*/	["Lavorò come volontario insieme alle crocerossine cremonesi nel 1916, chi fu?",
+    			"risposta giusta"], //risposta giusta
+    /*21*/	["Chi, molto caro alle sue sorelle, è raffigurato con l'elmo di Scipio?",
+    			"risposta giusta"], //risposta giusta
+    /*22*/	["Il giusto dell'anima è colui che si dona al prossimo, chi è degno di questo premio?",
+    			"risposta giusta"], //risposta giusta
+    /*23*/	["Chi morì con il grado maggiore di cavalleria dopo aver fatto il soldato per 15 anni?",
+    			"risposta giusta"], //risposta giusta
+    /*24*/	["Quale lapide raffigura due giberne appese allo scudo dei Savoia?", 
+    			"risposta giusta"], //risposta giusta
 ];
