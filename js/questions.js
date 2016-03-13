03e15440b4c719fc5aacda729b11cdfa6bc36429
Mattia Portanti <Mattia Portanti>
2016-03-13 12:24:53 +0100

    modificato codice scrpt.js domane; aggiunto tipo domande

--- js/questions.js
+++ js/questions.js
@@ -1,100 +1,124 @@
 var domande = [
 	/*0*/	"0",
 	/*1*/	["1",
+                "1", //numero risposte
                 "Giorgio Rossi", //risposta giusta
                 "Antonio Ciboldi"  //autore
             ],
 
     /*2*/	["Quale lapide ha qualcosa in comune con il film ”Caccia ad ottobre rosso” e parla di un nuovo mezzo marittimo?",
+                "1", //numero risposte
                 "Annibale Eugenio", //risposta giusta
                 "Riccardo Pontevichi" //autore
             ],
 
     /*3*/	["Quali sono le due lapidi che sono “decorate” con l'immagine di un fucile?",
+                "2", //numero risposte
                 ["Ettore Galli", "Luigi Marconi"],  //risposta giusta
                 "Andrea Marca" //autore
             ],
     /*4*/	["In quante lapidi al posto del fucile hanno messo la spada?",
+                "3", //numero risposte
                 ["Quattro", "4"], //risposta giusta
                 "Gianmarco Villani" //autore
             ],
     /*5*/	["Trova il caporale del 35° regg. Fanteria disperso durante il fatto d'armi",
+                "1", //numero risposte
                 "Annibale Cassani", //risposta giusta
                 "Luigi Tamb&egrave;" //autore
             ],
     /*6*/	["Quali sono i simboli della guerra più ricorrenti nelle decorazioni delle lapidi?",
+                "1", //numero risposte
                 "risposta giusta", //risposta giusta sasdadsdasadsasdsadasdaasdadsasdasdasdasdsadasasdasda da guardare
                 "Matteo Lorandi" //autore
             ],
     /*7*/	["Nel nostro cimitero sono sepolti solo i soldati italiani?",
+                "1", //numero risposte
                 "no", //risposta giusta
                 "Matteo Lucini" //autore
             ],
     /*8*/	["Quale particolare arma chimica $egrave; stata utilizzata nella prima guerra mondiale? Di quale patriota ha causato la morte?",
+                "1", //numero risposte
                 "i gas asfissiante, Giovanni Bardelli", //risposta giusta aslkdasjsalkadsjasdlkdsajasldkj da guardare
                 "Arianna Grazioli" //autore
             ],
     /*9*/	["Chi insegn&ograve; ai suoi compagni la lingua tedesca e fu tra i primi a prestare giuramento nella Guardia Nazionale di Cremona?",
+                "1", //numero risposte
                 "Giuseppe Boselli", //risposta giusta
                 "Letizia Morello" //autore
             ],
     /*10*/	["Rinunci&ograve; alla sua giovinezza per un'Italia più grande e morì lontano dai genitori. Chi è ?",
+                "1", //numero risposte
                 "Ernesto Stagnati", //risposta giusta
                 "Valeria Gallo" //autore
             ],
     /*11*/	["Chi fu il sottotenente dei bersaglieri che cadde combattendo sul Carnizza?",
+                "1", //numero risposte
                 "Luigi Marconi", //risposta giusta
                 "Stella Gardani" //autore
             ],
     /*12*/	["Chi &egrave; il più giovane tra i caduti della prima guerra mondiale nelle fotografie della mostra?",
+                "1", //numero risposte
                 "Giosafat Priori", //risposta giusta
                 "Alessandro Soavi" //autore
             ],
     /*13*/	["Chi &egrave; rappresentato sulla sua lapide portando la bandiera italiana nonostante sia nato all'estero?",
+                "1", //numero risposte
                 "Giuseppe Pelegri ", //risposta giusta
                 "Eleonora Ghisolfi" //autore
             ],
     /*14*/	["Durante la prima guerra mondiale scomparve nel mare insieme al sommergibile nel quale svolgeva il compito di secondo capo meccanico. Di chi si tratta?",
+                "1", //numero risposte
                 "Annibale Eugenio Amici", //risposta giusta
                 "Irene Ferri" //autore
             ],
     /*15*/	["Qual &egrave; il personaggio assistito da “Miss uragano” durante la battaglia del Volturno?",
+                "1", //numero risposte
                 "Francesco Pozzi", //risposta giusta
                 "Marco Racioppi" //autore
             ],
     /*16*/	["Tra le foto delle lapidi, ce n'&egrave; una in memoria di due fratelli, entrambi nati a Cremona verso la fine del XIX secolo: uno &grave; stato ucciso da una granata nemica, l'altro &egrave; deceduto a causa di una malattia. Sapresti trovarla?",
+                "1", //numero risposte
                 "I fratelli Ardenghi", //risposta giusta    lkadaslkdasdaslkdjdsalkjdskjsadbadskjdsa da guardare
                 "Marco Fiameni" //autore
             ],
     /*17*/	["Chi dopo 30 mesi di guerra mor&igrave; dopo tanti sacrifici il 30 maggio del 1919 all'ospedale Manini?",
+                "1", //numero risposte
                 "Palmiro Balestrieri", //risposta giusta
                 "Filippo Mariani" //autore
             ],
     /*18*/	["Schierato per un esercito rivale, disert&ograve; per combattere con l'Italia. Non conosciamo il suo volto. Chi &egrave; ?",
+                "1", //numero risposte
                 "Giuseppe Boselli", //risposta giusta
                 "Gabriele Speroni" //autore
             ],
     /*19*/	["Trova quante volte, con i rispettivi nomi dei caduti, è presente sulle lapidi il simbolo dei Savoia rappresentato da una croce e una corona",
+                "3", //numero risposte
                 ["Tre", "3"], //risposta giusta
                 "Simone Marchiaro" //autore
             ],
     /*20*/	["Lavor&ograve; come volontario insieme alle crocerossine cremonesi nel 1916, chi fu?",
+                "1", //numero risposte
                 "Angelo Dadomo", //risposta giusta
                 "Anna Cresceri" //autore
             ],
     /*21*/	["Chi, molto caro alle sue sorelle, &egrave; raffigurato con l'elmo di Scipio?",
+                "1", //numero risposte
                 "Lavinio Zanacchi ", //risposta giusta
                 "Anna Cresceri" //autore
             ],
     /*22*/	["Il giusto dell'anima &egrave; colui che si dona al prossimo, chi &egrave; degno di questo premio?",
+                "1", //numero risposte
                 "Angelo Dadomo", //risposta giusta
                 "Giulia Mazzetti" //autore
             ],
     /*23*/	["Chi mor&igrave; con il grado maggiore di cavalleria dopo aver fatto il soldato per 15 anni?",
+                "1", //numero risposte
                 "Enrico Binda", //risposta giusta
                 "Benedetta Bolsi" //autore
             ],
     /*24*/	["Quale lapide raffigura due giberne appese allo scudo dei Savoia?", 
+                "1", //numero risposte
                 "Ernesto Lucchini", //risposta giusta
                 "Giovanni Vittori" //autore
             ],
